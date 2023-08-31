8b922520d660bd3f065f488b02170899754a2ebb
Caio E. F. de Miranda <116667039+Aushiffer@users.noreply.github.com>
2023-08-30 22:18:31 -0300

    Add files via upload

---
+++ T1_conjuntos/Set.c
@@ -0,0 +1,223 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "Set.h"
+
+bool isEmpty(struct set s) {
+        if (s.size == 0)
+                return true;
+        return false;
+} // retorna true se S for vazio
+
+int size(struct set s) {
+        int count, i;
+        s.elements[s.size] = -1;
+        i = 0;
+        count = 0;
+        while (s.elements[i] != -1) {
+                count++;
+                i++;
+        } // até que o elemento em s.elements[i] seja -1, count++ e i++
+
+        return count;
+}
+
+bool isIn(struct set s, int x) {
+        for (int i = 0; i < s.size; i++)
+                if (s.elements[i] == x)
+                        return true;
+        return false;
+} // busca linear que ocorre até que se ache x (ou não)
+
+void printSet(struct set s) {
+        if (isEmpty(s)) { // verifica se S é vazio
+                printf("conjunto vazio\n");
+        } else {
+                int aux;
+                s.elements[s.size] = -1; // evitando a impressão de lixo de memória em um caso especial
+                for (int i = 0; i < s.size; i++)
+                        for (int j = i + 1; j < s.size; j++) {
+                                if (s.elements[i] == s.elements[j])
+                                        s.elements[j] = -1;
+                                if (s.elements[i] < s.elements[j]) {
+                                        aux = s.elements[i];
+                                        s.elements[i] = s.elements[j];
+                                        s.elements[j] = aux;
+                                }
+                        } // -1 em duplicatas e ordenação decrescente
+                s.size = size(s); // remoção de duplicatas
+                for (int i = 0; i < s.size; i++)
+                        printf("%d ", s.elements[i]);
+                printf("\n");
+        } // for loop que imprime todos os elementos de S
+}
+
+void sortSet(struct set s) {
+        int aux; // variável auxiliar para a troca
+        if (isEmpty(s)) { // verifica se S é vazio
+                printf("conjunto vazio\n");
+        } else {
+                s.elements[s.size] = -1; // evitando a impressão de lixo de memória em um caso especial
+                for (int i = 0; i < s.size; i++)
+                        for (int j = i + 1; j < s.size; j++) {
+                                if (s.elements[i] == s.elements[j])
+                                        s.elements[j] = -1;
+                                if (s.elements[i] < s.elements[j]) {
+                                        aux = s.elements[i];
+                                        s.elements[i] = s.elements[j];
+                                        s.elements[j] = aux;
+                                }
+                        } // -1 em duplicatas e ordenação decrescente
+                s.size = size(s); // remoção de duplicatas
+                for (int i = 0; i < s.size; i++)
+                        for (int j = i + 1; j < s.size; j++)
+                                if (s.elements[i] > s.elements[j]) {
+                                        aux = s.elements[i];
+                                        s.elements[i] = s.elements[j];
+                                        s.elements[j] = aux;
+                                } // fim da ordenação crescente
+                for (int i = 0; i < s.size; i++)
+                        printf("%d ", s.elements[i]);
+                printf("\n"); // impressão dos elementos de S em ordem crescente
+        }
+}
+
+void printIntersection(struct set s1, struct set s2) {
+        struct set isec;
+        int aux; // var. auxiliar para a troca (ordenação decrescente)
+        isec.size = MAX_SIZE;
+        for (int i = 0; i < MAX_SIZE; i++)
+                isec.elements[i] = -1; // inicialização de isec.elements[] com -1
+        for (int i = 0; i < s1.size; i++)
+                for (int j = 0; j < s2.size; j++)
+                        if (s1.elements[i] == s2.elements[j])
+                                isec.elements[i] = s1.elements[i]; // coloca elementos coincidentes em isec.elements[]
+        for (int i = 0; i < isec.size; i++)
+                for (int j = i + 1; j < isec.size; j++) {
+                        if (isec.elements[i] == isec.elements[j])
+                                isec.elements[j] = -1;
+                        if (isec.elements[i] < isec.elements[j]) {
+                                aux = isec.elements[i];
+                                isec.elements[i] = isec.elements[j];
+                                isec.elements[j] = aux;
+                        }
+                } // ordenação decrescente e -1 em duplicatas
+        isec.size = size(isec); // definição do tamanho de isec usando a função size (consequentemente remove as duplicatas)
+        printf("imprimindo intersecção...\n");
+        printSet(isec); // impressão da intersecção usando printSet
+}
+
+void printUnion(struct set s1, struct set s2) {
+	struct setUni {
+                int size;
+                int elements[s1.size + s2.size];
+        }; // struct da união, para evitar que MAX_SIZE seja ultrapassado
+
+        int aux; // var. auxiliar para a troca
+	if (s1.size == 0) {
+                printf("imprimindo união...\n");
+                s2.elements[s2.size] = -1; // evitando a impressão de lixo de memória
+                for (int i = 0; i < s2.size; i++)
+                        for (int j = i + 1; j < s2.size; j++) {
+                                if (s2.elements[i] == s2.elements[j])
+                                        s1.elements[j] = -1;
+                                if (s2.elements[i] < s2.elements[j]) {
+                                        aux = s2.elements[i];
+                                        s2.elements[i] = s2.elements[j];
+                                        s2.elements[j] = aux;
+                                } // ordenação decrescente e -1 em duplicatas
+                        }
+                s2.size = size(s2); // remoção de duplicatas
+		printSet(s2); // se s1 for vazio, s2 é impresso
+	} else if (s2.size == 0) {
+                printf("imprimindo união...\n");
+                s1.elements[s1.size] = -1; // evitando a impressão de lixo de memória
+                for (int i = 0; i < s1.size; i++)
+                        for (int j = i + 1; j < s1.size; j++) {
+                                if (s1.elements[i] == s1.elements[j])
+                                        s1.elements[j] = -1;
+                                if (s1.elements[i] < s1.elements[j]) {
+                                        aux = s1.elements[i];
+                                        s1.elements[i] = s1.elements[j];
+                                        s1.elements[j] = aux;
+                                }
+                        } // ordenação decrescente e -1 em duplicatas
+                s1.size = size(s1); // remoção de duplicatas
+                printSet(s1); // se s2 for vazio, s1 é impresso
+        } else {
+		struct setUni uni;
+		int k;
+		k = 0;
+		uni.size = s1.size + s2.size;
+                uni.elements[uni.size] = -1; // evitando a impressão de lixo de memória em um caso especial
+		for (int i = 0; i < s1.size; i++)
+			uni.elements[i] = s1.elements[i];
+		for (int i = s1.size; i < uni.size; i++) {
+			uni.elements[i] = s2.elements[k];
+			k++;
+		} // inserção dos elementos de ambos os conjuntos em uni.elements[]
+		for (int i = 0; i < uni.size; i++)
+			for (int j = i + 1; j < uni.size; j++) {
+				if (uni.elements[i] == uni.elements[j])
+                                        uni.elements[j] = -1;
+                                if (uni.elements[i] < uni.elements[j]) {
+					aux = uni.elements[i];
+					uni.elements[i] = uni.elements[j];
+					uni.elements[j] = aux;
+				}
+                        } // ordenação decrescente e -1 em duplicatas
+                k = 0;
+		printf("imprimindo união...\n");
+		while (uni.elements[k] != -1) {
+			printf("%d ", uni.elements[k]);
+			k++;
+		} /* até que uni.elements[k] seja -1, uni.elements[k] é impresso e k++
+		   * impressão sem usar printSet, uma vez que size e printSet aceitam
+		   * apenas struct set */
+		printf("\n");
+	}
+}
+
+void printDifference(struct set s1, struct set s2) {
+        int aux; // var. auxiliar para a troca
+        s1.elements[s1.size] = -1; // evitando a impressão de lixo de memória em um caso especial
+        for (int i = 0; i < s2.size; i++)
+                for (int j = 0; j < s1.size; j++)
+                        if (s2.elements[i] == s1.elements[j])
+                                s1.elements[j] = -1; // -1 em elementos de s2 que coincidem com elementos de s1
+        for (int i = 0; i < s1.size; i++)
+                for (int j = i + 1; j < s1.size; j++) {
+                        if (s1.elements[i] == s1.elements[j])
+                                s1.elements[j] = -1;
+                        if (s1.elements[i] < s1.elements[j]) {
+                                aux = s1.elements[i];
+                                s1.elements[i] = s1.elements[j];
+                                s1.elements[j] = aux;
+                        }
+                } // ordenação decrescente e -1 em duplicatas
+        s1.size = size(s1); // definição de s1.size usando size (consequentemente remove as duplicatas)
+        printf("imprimindo diferença...\n");
+        printSet(s1); // impressão da diferença usando printSet
+}
+
+bool isSubset(struct set s1, struct set s2) {
+        int aux; // var. auxiliar para a troca
+        s1.elements[s1.size] = -1; // evitando a impressão de lixo de memória em um caso especial
+        for (int i = 0; i < s2.size; i++)
+                for (int j = 0; j < s1.size; j++)
+                        if (s2.elements[i] == s1.elements[j])
+                                s1.elements[j] = -1; // -1 em elementos de s2 que coincidem com elementos de s1
+        for (int i = 0; i < s1.size; i++)
+                for (int j = i + 1; j < s1.size; j++) {
+                        if (s1.elements[i] == s1.elements[j])
+                                s1.elements[j] = -1;
+                        if (s1.elements[i] < s1.elements[j]) {
+                                aux = s1.elements[i];
+                                s1.elements[i] = s1.elements[j];
+                                s1.elements[j] = aux;
+                        }
+                } // ordenação decrescente e -1 em duplicatas
+        s1.size = size(s1); // definição de s1.size usando size
+        if (isEmpty(s1)) // verifica se s1 - s2 é vazia (se for, s1 é subconjunto de s2)
+                return true;
+        return false;
+}
---
+++ T1_conjuntos/mainSet.c
@@ -0,0 +1,58 @@
+/* Nome: Caio Eduardo Ferreira de Miranda; GRR: 20232359
+ * Arquivo para teste da biblioteca Set.h */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "Set.h"
+
+int main() {
+        struct set conj1, conj2;
+        int n, m;
+        scanf("%d %d", &n, &m);
+        printf("\n");
+        if ((n > MAX_SIZE || n < 0) || (m > MAX_SIZE || m < 0)) {
+                printf("erro: tamanho inválido de conjunto\n");
+        } else {
+                conj1.size = n;
+                conj2.size = m;
+                srand(time(NULL));
+                for (int i = 0; i < conj1.size; i++)
+                        conj1.elements[i] = rand() % 10;
+                for (int i = 0; i < conj2.size; i++)
+                        conj2.elements[i] = rand() % 10;
+                printf("imprimindo s1...\n");
+                printSet(conj1);
+                printf("imprimindo s2...\n");
+                printSet(conj2); /* o tamanho de conj1 e conj2 de fato
+                                  * é n e m, mas duplicatas não foram
+                                  * consideradas, já que são conjuntos */
+                printDifference(conj1, conj2);
+                printUnion(conj1, conj2);
+                printIntersection(conj1, conj2);
+                if (isIn(conj1, n)) {
+                        printf("\n%d pertence a s1\n", n);
+                } else {
+                        printf("\n%d não pertence a s1\n", n);
+                }
+                if (isIn(conj2, m)) {
+                        printf("%d pertence a s2\n", m);
+                } else {
+                        printf("%d não pertence a s2\n", m);
+                }
+                if (isSubset(conj1, conj2)) {
+                        printf("s1 é subconjunto de s2\n\n");
+                } else {
+                        printf("s1 não é subconjunto de s2\n\n");
+                }
+                printf("imprimindo conj1 em ordem crescente...\n");
+                sortSet(conj1);
+                printf("imprimindo conj2 em ordem crescente...\n");
+                sortSet(conj2);
+                printf("\no tamanho de s1 é: %d\n", size(conj1));
+                printf("o tamanho de s2 é: %d\n\n", size(conj2));
+        }
+        
+        return 0;
+}
+
