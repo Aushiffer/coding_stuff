d0c89020bfcd66c26616c836c3c24ee19389eccf
Caio E. F. de Miranda <116667039+Aushiffer@users.noreply.github.com>
2023-09-18 17:25:22 -0300

    Update ordenacao.c

--- Template Alunos/ordenacao.c
+++ Template Alunos/ordenacao.c
@@ -15,16 +15,25 @@ unsigned int getGRR(){
 
 int buscaSequencial(int vetor[], int tam, int valor, long* numComparacoes){	
 	if (tam < 0)
-		return -1;
-	*numComparacoes = *numComparacoes + 1;
+		return -1; // caso base
+	*numComparacoes = *numComparacoes + 1; // +1 comparação
 	if (valor == vetor[tam])
-		return tam;
+		return tam; // achou o valor especificado
 
-	return buscaSequencial(vetor, tam - 1, valor, numComparacoes);
+	return buscaSequencial(vetor, tam - 1, valor, numComparacoes); // chamada recursiva
 }
 
 int buscaBinaria(int vetor[], int tam, int valor, long* numComparacoes){
-	return -1;
+	int ini = (tam - 1) + 1 - tam; // inicio do vetor
+	if (ini > tam)
+		return -1; // caso base
+	int meio = (ini + tam) / 2;
+	*numComparacoes = *numComparacoes + 1; // +1 comparação abaixo
+	if (vetor[meio] > valor)
+		return buscaBinaria(vetor, meio - 1, valor, numComparacoes);
+		// chamada recursiva para um vetor de tamanho = meio - 1
+
+	return buscaBinaria(vetor, tam, valor, numComparacoes);
 }
 
 void swap(int *a, int *b) {
@@ -33,36 +42,36 @@ void swap(int *a, int *b) {
 	*b = aux; 
 }
 
-int minValue(int vetor[], int tam) {
-	int min = vetor[0];
+int maxValue(int vetor[], int tam) {
+	int max = vetor[0]; // assumindo vetor[0] como max
 	for (int i = 1; i < tam; i++)
-		if (vetor[i] < min)
-			min = vetor[i];
+		if (vetor[i] > max)
+			max = vetor[i]; // for loop que atualiza o valor de max conforme o if
 
-	return min;
+	return max; // maior elemento no vetor
 }
 
 long insertionSort(int vetor[], int tam){
 	if (tam <= 1)
-		return 0;
-	long comps = insertionSort(vetor, tam - 1);
-	int i = tam;
-	while (i > 0 && vetor[i] < vetor[i - 1]) {
+		return 0; // caso base, 0 comparações
+	long comps = insertionSort(vetor, tam - 1); // recursão, guarda as comparações das demais chamadas
+		for (int i = tam - 1; i > 0; i--) {
+		if (vetor[i] < vetor[i - 1])
+			swap(&vetor[i], &vetor[i - 1]);
 		comps++;
-		swap(&vetor[i], &vetor[i - 1]);
-		i--;
 	}
-			
+
 	return comps;
 }
 
 long selectionSort(int vetor[], int tam){
 	if (tam <= 1)
-		return 0;
-	
-	int comps = selectionSort(vetor, tam - 1);
+		return 0; // caso base, 0 comparações
+	int posMax = maxValue(vetor, tam);
+	swap(&vetor[tam - 1], &vetor[posMax]); // troca o último elemento com o maior
+	int comps = tam;
 
-	return comps;
+	return comps + selectionSort(vetor, tam - 1); // retorna o total de comparações
 }
 
 long mergeSort(int vetor[], int tam){
