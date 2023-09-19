41e41152813cace16e43e75f9eec9441551a879a
Caio E. F. de Miranda <116667039+Aushiffer@users.noreply.github.com>
2023-09-19 17:20:43 -0300

    Update main.c

--- Template Alunos/main.c
+++ Template Alunos/main.c
@@ -1,30 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 #include "ordenacao.h"
+#include "complementares.h"
 
-int main(){
+int main() {
 	char nome[MAX_CHAR_NOME];
-	int valSearch, *vetorTeste;
-	long size;
+	int *vetorTeste, valSearch;
+	long size, *numComps;
+	*numComps = 0;
 	getNome(nome);
 	printf("Nome: %s\n", nome);
 	printf("GRR: %u\n", getGRR());
-	scanf("%ld", &size);
-	vetorTeste = malloc(size * sizeof(int));
-	if (vetorTeste == NULL) {
-		printf("[-] Erro fatal: erro de alocação dinâmica\n");
+	scanf("%ld %d", &size, &valSearch);
+	if (!(vetorTeste = malloc(size * sizeof(int)))) {
+		printf("[-] ERRO FATAL: problema com alocação dinâmica de memória\n");
 		return 1;
 	}
 	srand(time(NULL));
 	for (int i = 0; i < size; i++)
-		vetorTeste[i] = rand() % 50;
+		vetorTeste[i] = rand() % 11;
 	printArray(vetorTeste, size);
-	insertionSort(vetorTeste, size);
+	selectionSort(vetorTeste, size);
 	printArray(vetorTeste, size);
+	int posValSearch = buscaSequencial(vetorTeste, size, valSearch, numComps); 
 	printf("comps ins. sort: %ld\n", insertionSort(vetorTeste, size));
-
+	printf("comps sel. sort: %ld\n", selectionSort(vetorTeste, size));
+	printf("searching for %d: %d\n", valSearch, posValSearch);
+	if (posValSearch == -1)
+		printf("%d isn't on the array\n", valSearch);
+	printf("numComps for seq. search: %ld\n", *numComps);
 	free(vetorTeste);
 	vetorTeste = NULL;
 
