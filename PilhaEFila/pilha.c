530b89fedb7d0591f3b0dfcfc762a76035eae13b
Caio E. F. de Miranda <116667039+Aushiffer@users.noreply.github.com>
2023-09-20 11:55:31 -0300

    Update pilha.c

--- PilhaEFila/pilha.c
+++ PilhaEFila/pilha.c
@@ -1,32 +1,19 @@
 #include <stdlib.h>
-
-int emptyStack(struct pilha *p) {
-	if (p -> itens[0] == -1)
-		return 1;
-
-	return 0;
-}
-
-int searchStack(struct pilha *p, int x) {
-	if (emptyStack(p) == 1) {
-		printf("[-] Pilha vazia\n");
-		return -1;
-	} else {
-		for (int i = 0; i < top; i++)
-			if (p -> itens[i] == x)
-				return i;
-	}
-	printf("[-] Elemento não encontrado\n");
-	
-	return -2;
-}
+#include "pilha.h"
 
 void push(struct pilha *p, int x) {
-	if (p -> top == cap) {
-		printf("[-] Overflow\n");
-		return;
-	} else {
-		p -> top = p -> top + 1;
-		p -> itens[p -> top] = x;
-	}
+	p -> top = p -> top + 1;
+	p -> itens[p -> top] = x;
+}
+
+void pop(struct pilha *p) {
+	p -> itens[p -> top] = -1;
+	p -> top = p -> top - 1;
+}
+
+int emptyStack(struct pilha *p) {
+	if (p -> top = -1)
+		return 1;
+	
+	return 0;
 }
