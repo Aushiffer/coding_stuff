0bcd3c654e41d8ef0c45ee6e77196a18ffb11191
Caio E. F. de Miranda <116667039+Aushiffer@users.noreply.github.com>
2023-09-20 21:48:45 -0300

    Update pilha.c

--- PilhaEFila/pilha.c
+++ PilhaEFila/pilha.c
@@ -1,19 +1,28 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "pilha.h"
 
 void push(struct pilha *p, int x) {
+	if (p -> top == p -> cap) {
+		printf("[-] Overflow detectado, abortando a execução...\n");
+		return;
+	}
 	p -> top = p -> top + 1;
 	p -> itens[p -> top] = x;
 }
 
-void pop(struct pilha *p) {
-	p -> itens[p -> top] = -1;
-	p -> top = p -> top - 1;
-}
-
-int emptyStack(struct pilha *p) {
-	if (p -> top = -1)
+int emptyStack(struct pilha p) {
+	if (p.top == 0)
 		return 1;
 	
 	return 0;
+}
+
+void pop(struct pilha *p) {
+	if (emptyStack(*) == 1) {
+		printf("[-] Underflow detectado, abortando a execução...\n");
+		return;
+	}
+	p -> itens[p -> top] = -1;
+	p -> top = p -> top - 1;
 }
