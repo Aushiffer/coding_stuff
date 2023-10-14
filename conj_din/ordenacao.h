c896d8a09e612f345639e8f3a8009c512cf45d98
Caio E. F. de Miranda <116667039+Aushiffer@users.noreply.github.com>
2023-10-14 11:42:04 -0300

    Delete conj_din directory

--- conj_din/ordenacao.h
+++
@@ -1,7 +0,0 @@
-void swap(int vetor[], int a, int b);
-
-int buscaSequencial(int vetor[], int a, int b, int x);
-
-void merge(int vetor[], int a, int b, int m);
-
-void mergeSort(int vetor[], int a, int b);
