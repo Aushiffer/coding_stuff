ebe2984f84c4e4c9a71a1e93a1c2ffc01951ebfd
Caio E. F. de Miranda <116667039+Aushiffer@users.noreply.github.com>
2023-11-22 01:54:48 -0300

    Add files via upload

---
+++ ordenacaoAlgII_2/complementar.c
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "complementar.h"
+
+void trocar(int *vetor, int a, int b) {
+        int aux = vetor[a];
+        vetor[a] = vetor[b];
+        vetor[b] = aux;
+}
+
+void printVetor(int *vetor, int b) {
+	for (int i = 0; i < b; i++)
+		printf("%d ", vetor[i]);
+	printf("\n");
+}
+
+void aleatorizarVetor(int *vetor, int b) {
+	for (int i = 0; i < b; i++)
+		vetor[i] = rand() % RNG_CAP;
+}
+
+int nodoEsq(int idxNodo) { return (idxNodo << 1) + 1; }
+
+int nodoDir(int idxNodo) { return (idxNodo << 1) + 2; }
+
+void maxHeapify(int *heap, int idxNodo, int tam, unsigned long *comps) {
+        int esq = nodoEsq(idxNodo);
+        int dir = nodoDir(idxNodo);
+        int max;
+        if (esq <= tam && heap[esq] > heap[idxNodo])
+                max = esq;
+        else
+                max = idxNodo;
+        if (dir <= tam && heap[dir] > heap[max])
+                max = dir;
+	*comps = *comps + 2;
+        if (max != idxNodo) {
+                trocar(heap, idxNodo, max);
+                maxHeapify(heap, max, tam, comps);
+        }
+}
+
+void constroiMaxHeap(int *vetor, int n, unsigned long *comps) {
+	for (int i = (n / 2); i >= 0; i--)
+		maxHeapify(vetor, i, n, comps);
+}
+
+void hpSort(int *vetor, int n, unsigned long *comps) {
+	constroiMaxHeap(vetor, n, comps);
+	for (int i = n; i >= 1; i--) {
+		trocar(vetor, 0, i);
+		maxHeapify(vetor, 0, i - 1, comps);
+	}
+}
+
+int particaoQS(int *vetor, int a, int b, unsigned long *comps) {
+        if (a >= b)
+                return 0;
+        int pivo = vetor[b - 1];
+        int novoPivo = a;
+        for (int i = a; i < b - 1; i++) {
+                (*comps)++;
+		if (vetor[i] <= pivo) {
+                        trocar(vetor, novoPivo, i);
+                        novoPivo++;
+                }
+	}
+        trocar(vetor, novoPivo, b - 1);
+
+        return novoPivo;
+}
+
+void qkSort(int *vetor, int a, int b, unsigned long *comps) {
+	if (a >= b)
+		return;
+	int idxPivo = particaoQS(vetor, a, b, comps);
+	qkSort(vetor, a, idxPivo, comps);
+	qkSort(vetor, idxPivo + 1, b, comps);
+}
+
+void merge(int *vetor, int a, int b, int meio, unsigned long *comps) {
+	if (a >= b)
+		return;
+	int i, j, k, *vetorAux;
+	vetorAux = (int *)malloc(b * sizeof(int));
+        i = a;
+	j = meio + 1;
+	k = a;
+	while (i < meio && j < b) {
+		(*comps)++;
+		if (vetor[i] <= vetor[j]) {
+			vetorAux[k] = vetor[i];
+			i++;
+			k++;
+		} else {
+			vetorAux[k] = vetor[j];
+			j++;
+			k++;
+		}
+	}
+	while (i <= meio) {
+		vetorAux[k] = vetor[i];
+		i++;
+		k++;
+	}
+	while (j < b) {
+		vetorAux[k] = vetor[j];
+		j++;
+		k++;
+	}
+	for (int p = a; p < b; p++)
+		vetor[p] = vetorAux[p];
+        free(vetorAux);
+        vetorAux = NULL;
+}
+
+void mrgSort(int *vetor, int a, int b, unsigned long *comps) {
+        if (a >= b)
+                return;
+        int meio = (a + b) / 2;
+        mrgSort(vetor, a, meio, comps);
+        mrgSort(vetor, meio + 1, b, comps);
+        merge(vetor, a, b, meio, comps);
+}
---
+++ ordenacaoAlgII_2/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "ordenacao.h"
+#include "complementar.h"
+
+int main() {
+	srand(time(NULL));
+        char nome[MAX_CHAR];
+        char nomeAlg[MAX_CHAR];
+        getNome(nome);
+	getNomeAlgoritmoExtra(nomeAlg);
+        printf("Aluno: %s\n", nome);
+        printf("GRR: %u\n", getGRR());
+	printf("%s\n", nomeAlg);
+
+        unsigned int *vetor, tamVetor;
+	unsigned long numComps;
+	printf("Tamanho do vetor a ser ordenado: ");
+	scanf("%d", &tamVetor);
+        if (!(vetor = (unsigned int *)malloc(tamVetor * sizeof(unsigned int)))) {
+		printf("[-] Erro na alocação do vetor.\n");
+		return -1;
+	}
+	vetor[0] = 3;
+	vetor[1] = 2;
+	vetor[2] = 10;
+	vetor[3] = 1;
+	vetor[4] = 6;
+	vetor[5] = 6;
+	for (int i = 0; i < tamVetor; i++)
+		printf("%d ", vetor[i]);
+	printf("\n");
+	countingSort(vetor, tamVetor, 10);
+	for (int j = 0; j < tamVetor; j++)
+		printf("%d ", vetor[j]);
+	printf("\n");
+	// printf("comps: %ld\n", numComps);
+	free(vetor);
+	vetor = NULL;
+
+	return 0;
+}
