744dd497ab7e48c3eb87e77c1b4477bb95ef8a5c
Caio E. F. de Miranda <116667039+Aushiffer@users.noreply.github.com>
2024-03-05 09:55:32 -0300

    Update jornal.c

--- jornal_dinf/jornal.c
+++ jornal_dinf/jornal.c
@@ -53,8 +53,6 @@ void seleciona_noticias(fila *f, int *n_noticias) {
 			} else if (!noticia_valida(aux)) {
 				if (aux->prox != NULL)
 					aux = aux->prox;
-
-				remove_fila(f);
 			}
 
 			count++;
